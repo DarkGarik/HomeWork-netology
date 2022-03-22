c4fc221d9209c8800bfaadff67c61ea56dcaf1f0
Sergey Gorkov <sergey@gorkov.pro>
2022-03-22 20:09:50 +0300

    07-terraform-02-syntax end

--- 07-terraform-02-syntax/yandex-cloud-terraform/main.tf
+++ 07-terraform-02-syntax/yandex-cloud-terraform/main.tf
@@ -1,15 +1,40 @@
-terraform {
-  required_providers {
-    yandex = {
-      source = "terraform-registry.storage.yandexcloud.net/yandex-cloud/yandex"
-    }
-  }
-  required_version = ">= 0.13"
-}
-
 provider "yandex" {
   token     = "${var.yc_token}"
   cloud_id  = "${var.yc_id}"
   folder_id = "${var.yc_folder_id}"
   zone      = "ru-central1-a"
+}
+resource "yandex_compute_instance" "vm-1" {
+  name = "netology"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd83n3uou8m03iq9gavu"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
+
+resource "yandex_vpc_network" "network-1" {
+  name = "network1"
+}
+
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = "subnet1"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
 }
