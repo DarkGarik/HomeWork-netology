18278760915420e9a07b616f4ad63940efc6b70b
Sergey Gorkov <sergey@gorkov.pro>
2022-03-26 21:30:05 +0300

    07-terraform-03-basic end

---
+++ 07-terraform-03-basic/terraform-workspaces/variables.tf
@@ -0,0 +1,11 @@
+variable "yc_token" {}
+variable "yc_id" {
+  default = "b1g32fqo1f048nbto80h"
+}
+variable "yc_folder_id" {
+  default = "b1gu7o9d03stfnjd4h0e"
+}
+variable name {
+  default = "netology"
+}
+
